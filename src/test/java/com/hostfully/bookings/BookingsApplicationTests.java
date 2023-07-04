f3a882ec3acf1d82095f7885a47f3f63c4dc7c0d
Gökhan Üstüner <gokhanustuner@Gokhans-MacBook-Pro.local>
2023-07-04 12:36:41 +0300

    Initial commit

---
+++ src/test/java/com/hostfully/bookings/BookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostfully.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
